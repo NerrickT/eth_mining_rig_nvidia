0c972c7e823e6c05f5478e75cb34aea41afec035
VincentV <vincent_valentine@outlook.com>
2017-08-29 03:00:47 -0400

    Fixing GPU2 fan speed

--- mine.sh
+++ mine.sh
@@ -3,8 +3,9 @@
 #`Only use the following on the initial startup of your rig.`
 #`sudo update-grub`
 #`sudo nvidia-xconfig -a --cool-bits=28 --allow-empty-initial-configuration` 
+#`sudo nvidia-xconfig -a --enable-all-gpus --cool-bits=31 --allow-empty-initial-configuration` 
 
-
+# Website ref: https://www.reddit.com/r/EtherMining/comments/6gbnw4/is_anyone_able_to_overclockp0_a_1060_under_linux/
 
 
 
@@ -34,9 +35,9 @@ nvidia-smi -i 1 -pl 93
 #GPU 3
 nvidia-settings -a '[gpu:2]/GPUMemoryTransferRateOffset[3]=710'
 nvidia-settings -a '[gpu:2]/GPUFanControlState=1'
-nvidia-settings -a '[fan:2]/GPUTargetFanSpeed=62'
+nvidia-settings -a '[fan:2]/GPUTargetFanSpeed=90'
 # Change Power (to check power settings: nvidia-smi -i 0 -q -d POWER)
 nvidia-smi -i 0 -pl 93
 
 
-./ethdcrminer64 -epool us1.ethermine.org:14444 -ewal <YOUR ETH ADDRESS GOES HERE>.<Rig NAME GOES HERE> -epsw x -mode 1 -eres 5 -tt 65,68  -allpools 1
+./ethdcrminer64 -epool us1.ethermine.org:14444 -ewal <YOUR ETH ADDRESS GOES HERE>.<Rig NAME GOES HERE> -epsw x -mode 1 -eres 5 -tt 65,68,69  -allpools 1
