e4abf7058d2fcab02083ba9d8375a21020c7d214
VincentV <vincent_valentine@outlook.com>
2018-01-01 18:30:36 -0500

    Made some changes to keep the mining going strong.

--- mine.sh
+++ mine.sh
@@ -17,27 +17,33 @@ export GPU_MAX_ALLOC_PERCENT=100
 export GPU_SINGLE_ALLOC_PERCENT=100
 
 #GPU 0
-nvidia-settings -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=1450'
+#nvidia-settings -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=1450'
+#nvidia-settings -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=1100'
+nvidia-settings -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=800'
 nvidia-settings -a '[gpu:0]/GPUFanControlState=1'
 nvidia-settings -a '[fan:0]/GPUTargetFanSpeed=62'
 # Change Power (to check power settings: nvidia-smi -i 0 -q -d POWER)
-nvidia-smi -i 0 -pl 93
+#nvidia-smi -i 0 -pl 93
 
 
 #GPU 1
-nvidia-settings -a '[gpu:1]/GPUMemoryTransferRateOffset[3]=1400'
+#nvidia-settings -a '[gpu:1]/GPUMemoryTransferRateOffset[3]=1300'
+#nvidia-settings -a '[gpu:1]/GPUMemoryTransferRateOffset[3]=1100'
+nvidia-settings -a '[gpu:1]/GPUMemoryTransferRateOffset[3]=800'
 # Change Power (to check power settings: nvidia-smi -i 1 -q -d POWER)
-nvidia-smi -i 1 -pl 93
+# nvidia-smi -i 1 -pl 93
 
 
 
 
-#GPU 3
-nvidia-settings -a '[gpu:2]/GPUMemoryTransferRateOffset[3]=710'
-nvidia-settings -a '[gpu:2]/GPUFanControlState=1'
-nvidia-settings -a '[fan:2]/GPUTargetFanSpeed=90'
-# Change Power (to check power settings: nvidia-smi -i 0 -q -d POWER)
-nvidia-smi -i 0 -pl 93
+##GPU 3
+#nvidia-settings -a '[gpu:2]/GPUMemoryTransferRateOffset[3]=710'
+#nvidia-settings -a '[gpu:2]/GPUFanControlState=1'
+#nvidia-settings -a '[fan:2]/GPUTargetFanSpeed=90'
+## Change Power (to check power settings: nvidia-smi -i 0 -q -d POWER)
+#nvidia-smi -i 0 -pl 93
 
 
-./ethdcrminer64 -epool us1.ethermine.org:14444 -ewal <YOUR ETH ADDRESS GOES HERE>.<Rig NAME GOES HERE> -epsw x -mode 1 -eres 5 -tt 65,68,69  -allpools 1
+#./ethdcrminer64 -epool us1.ethermine.org:14444 -ewal 6Be282d17902823f8913376195c84817B91Dc2e6.kaylyn_02 -epsw x -mode 1 -eres 5 -tt 65,68,69  -allpools 1
+
+/bin/Claymore/ethdcrminer64 -epool us1.ethermine.org:14444 -ewal 6Be282d17902823f8913376195c84817B91Dc2e6.kaylyn_02 -epsw x -mode 1 -eres 5 -tt 65,68,69  -allpools 1
